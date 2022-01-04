22a0c722c3185670c5b5ca2dcca9f094df9d5008
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2022-01-04 22:40:48 +0530

    kaacha baadam

---
+++ src/actions/posts.js
@@ -0,0 +1,26 @@
+import { UPDATE_POSTS } from './actionTypes';
+
+export function fetchPosts() {
+  console.log('inside did ');
+  return (dispatch) => {
+    //const url = 'http://codeial.codingninjas.com:8000/api/v2/posts';
+    const url =
+      'http://codeial.codingninjas.com:8000/api/v2/posts?page=1&limit=5';
+    fetch(url)
+      .then((response) => {
+        console.log('response==>', response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        dispatch(updatePosts(data.data.posts));
+      });
+  };
+}
+
+export function updatePosts(posts) {
+  return {
+    type: UPDATE_POSTS,
+    posts,
+  };
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,27 @@
-function App() {
-  return <div className="App">Apps</div>;
+// function App() {
+//   return <div className="App">Apps</div>;
+// }
+
+// creatinga function componenet
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/posts';
+class App extends React.Component {
+  componentDidMount() {
+    console.log('sda');
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    console.log('PRPOS', this.props);
+    return <div>App</div>;
+  }
 }
 
-export default App;
+function mapStareTOProps(state) {
+  return {
+    posts: state.posts,
+  };
+}
+
+export default connect(mapStareTOProps)(App);
