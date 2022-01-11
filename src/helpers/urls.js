2a5f9879493edcb9a349175f8ab265d80456a23f
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2022-01-11 09:34:49 +0530

    user logged in 2

---
+++ src/helpers/urls.js
@@ -0,0 +1,11 @@
+const API_ROOT = 'http://codeial.codingninjas.com:8000/api/v2';
+export const APIUrls = {
+  login: () => `${API_ROOT}/users/login`,
+  sugnup: () => `${API_ROOT}/users/signup`,
+  fetchPosts: (
+    page = 1,
+    limit = 5 // here we can create call this function dynamically bby passing page and limits
+  ) =>
+    // this will return this url and when we called this function in thr post
+    `${API_ROOT}/posts?page=${page}&limit=${limit}`,
+};
