4cfb39391a5b65ff22a1dc0a3a6d16103c54346d
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2022-01-14 09:53:32 +0530

    aded steps for controlled component in js and implemented

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,25 +1,53 @@
-// for uncontrolled comp
-//1.creatre constrocutor
-//2. declare valriable like this   this.name = React.createRef();
-// 3. inthe input tag using ref do like this ref={this.name}
-//4.in the submit call the handleFormFunction using like this this.handleFormFunction
+//step: for comtrolled componenets
+//1.create state in the constructor and assign the property of satte  this.state = {name: '',}
+//2.create a handler for the property like this  handleNameChange = (e) => this.setState({name: e.target.value,});    };
+//3. in the input tag  <input placeholder="Name" type="text" placeholder="Name" required onChange={this.handleNameChange} value={this.state.name}          //ref={this.name}/>
+//4.here in the above we have add addChnge and value in the input tag
 import React, { Component } from 'react';
 
 class Signup extends Component {
   // implementation using comtroll comp
   constructor(props) {
     super(props);
-    this.name = React.createRef();
-    this.email = React.createRef();
-    this.confirmPassword = React.createRef();
-    this.password = React.createRef();
+    /*
+this.name = React.createRef();
+this.email = React.createRef();
+this.confirmPassword = React.createRef();
+this.password = React.createRef();*/
+    this.state = {
+      name: '',
+      email: '',
+      cnfPass: '',
+      password: '',
+    };
   }
+  handleNameChange = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+  handleCnfPasswordChange = (e) => {
+    this.setState({
+      cnfPass: e.target.value,
+    });
+  };
   handleFormSubmit = (e) => {
-    e.preventDefault();
-    console.log('name', this.name);
-    console.log('email', this.email);
-    console.log('confirm password', this.confirmPassword);
-    console.log('password', this.password);
+    e.preventDefault(); /*
+console.log('name', this.name);
+console.log('email', this.email);
+console.log('confirm password', this.confirmPassword);
+console.log('password', this.password);*/
+    console.log(this.state);
   };
   render() {
     return (
@@ -31,18 +59,28 @@ class Signup extends Component {
             type="text"
             placeholder="Name"
             required
-            ref={this.name}
+            onChange={this.handleNameChange}
+            value={this.state.name}
+            //ref={this.name}
           />
         </div>
         <div className="field">
-          <input placeholder="email" type="email" required ref={this.email} />
+          <input
+            placeholder="email"
+            type="email"
+            // required ref={this.email}
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+          />
         </div>
         <div className="field">
           <input
             placeholder="Confirm Password"
             required
             type="password"
-            ref={this.confirmPassword}
+            //   ref={this.confirmPassword}
+            onChange={this.handleCnfPasswordChange}
+            value={this.state.cnfPass}
           />
         </div>
         <div className="field">
@@ -50,7 +88,9 @@ class Signup extends Component {
             placeholder="Password"
             required
             type="password"
-            ref={this.password}
+            // ref={this.password}
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
           />
         </div>
         <div className="field">
